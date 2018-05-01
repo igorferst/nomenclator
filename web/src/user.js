a3a5484581970d7d29b8bfbe4dc9ec50fff56296
Igor Ferst <igor@fer.st>
2018-05-01 14:40:01 -0400

    More user function stubs

--- web/src/user.js
+++ web/src/user.js
@@ -1,7 +1,13 @@
+const login = function() {
+    return Promise.resolve({})
+};
 
+const logout = function() {
+    return Promise.resolve({})
+}
 
 const get = function() {
     return Promise.resolve({})
 }
 
-export {get};
+export {login, logout, get};
